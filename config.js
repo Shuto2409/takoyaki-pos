d9d4716aeb2b4bb6631ba34a5e386aed29ec420b
Shuto2409 <0924shuto@gmail.com>
2026-05-11 20:54:24 +0900

    Create  config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// 学園祭POSシステム Firebase設定
+// このファイルはindex.htmlと同じフォルダに置いてください
+window.PRESET_CONFIG = {
+  "apiKey": "AIzaSyC6jcQZk2Jw-SXuCQP4_W1tOZp1qakRwbc",
+  "authDomain": "hien-7c5ba.firebaseapp.com",
+  "databaseURL": "https://hien-7c5ba-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "projectId": "hien-7c5ba",
+  "storageBucket": "hien-7c5ba.firebasestorage.app",
+  "messagingSenderId": "32276340610",
+  "appId": "1:32276340610:web:a73d1b148e155fc8cbb99b"
+};
